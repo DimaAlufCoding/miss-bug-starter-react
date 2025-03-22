fdc0d95838179be4e5be0ac8ba3b314028a9f085
Dima <dima.aluf@gmail.com>
2025-03-22 16:33:09 +0200

    fixing type

--- miss-bug-proj/server.js
+++ miss-bug-proj/server.js
@@ -2,4 +2,4 @@ import express from 'express'
 const app = express()
 app.get('/', (req, res) => res.send('Hello there'))
 
-app.listen(3036, () => console.log('1Server ready at port 3036'))
+app.listen(3036, () => console.log('Server ready at port 3036'))
