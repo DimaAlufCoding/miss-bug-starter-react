5d739e164ec738c1e4fd222c618cc72ac0ca63e3
Dima <dima.aluf@gmail.com>
2025-03-22 17:50:20 +0200

    sever support of getting entities

--- miss-bug-proj/server.js
+++ miss-bug-proj/server.js
@@ -1,5 +1,23 @@
 import express from 'express'
+import { bugService } from '../services/bug.service.js'
+
 const app = express()
+app.listen(3036, () => console.log('Server ready at port 3036'))
+
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+        .then(bugs => res.json(bugs))
+})
+
+app.get('/api/bug/save', (req, res) => { })
+
+app.get('/api/bug/:bugId', (req, res) => { })
+
+app.get('/api/bug/:bugId/remove', (req, res) => { })
+
 app.get('/', (req, res) => res.send('Hello there'))
 
-app.listen(3036, () => console.log('Server ready at port 3036'))
+
+
+
